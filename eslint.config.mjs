053b9bb670fc664c8191215f1d60164332cdae1f
SinaTZ0 <149362011+SinaTZ0@users.noreply.github.com>
2025-05-26 21:49:09 +0330

    merge__b1_add-linting (#3)
    * fix(#1): adding some pnpm config so eslint works properly
    
    * chore(#1):
    - add unicorn eslint plugin.
    - add eslint-plugin-import-x eslint plugin.
    - add linting commands.
    - fix layout linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
+/* eslint-disable unicorn/prefer-node-protocol */
+/* eslint-disable unicorn/import-style */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import * as importX from "eslint-plugin-import-x";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +16,45 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginUnicorn.configs.recommended,
+  importX.flatConfigs.recommended,
+  importX.flatConfigs.typescript,
+  {
+    rules: {
+      "import-x/order": [
+        "error", // or 'error'
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+          pathGroups: [
+            {
+              pattern: "next",
+              group: "internal",
+              position: "before",
+            },
+            {
+              pattern: "next/**",
+              group: "internal",
+              position: "before",
+            },
+
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin", "external", "object"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import-x/no-deprecated": "error",
+      "import-x/first": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
